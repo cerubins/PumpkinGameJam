9264f8f73ad9ca908cef0753723f0a4ad47d87cb
- <->
2022-10-16 11:34:39 -0400

    some more hud stuff

--- Assets/Prefab/UI Assets/HUD.cs
+++ Assets/Prefab/UI Assets/HUD.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class HUD : MonoBehaviour
 {
     public static HUD instance;
-
+    public enum MenuType {GAME_OVER, MAIN_MENU};
     void Awake()
     {
         instance = this;
         DontDestroyOnLoad(gameObject);
+        changeToMenu(MenuType.MAIN_MENU);
     }
 
     // Update is called once per frame
@@ -17,4 +18,30 @@ public class HUD : MonoBehaviour
     {
         
     }
+
+    public void changeToMenu(MenuType menuType)
+    {
+        //Turn off all UI panels
+        foreach(Transform childTransform in transform)
+        {
+            childTransform.gameObject.active = false;
+        }
+
+        //Then turn on only the UI panel we want to switch to
+        switch (menuType)
+        {
+            case MenuType.MAIN_MENU:
+                transform.Find("Main Menu").gameObject.active = true;
+                break;
+            case MenuType.GAME_OVER:
+                transform.Find("Game Over").gameObject.active = true;
+                break;
+            default:
+                // code block
+                break;
+        }
+
+    }
+
+
 }
